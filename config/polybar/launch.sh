0414b7fefa840d9921dd6058d241ebf5790eab4b
Ryn <Ryan@Hannahs-MacBook-Pro-2.local>
2024-09-04 07:22:11 -0400

    Cleanup main

--- config/polybar/launch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-killall -q polybar
-
-polybar bar &
-
-if [[ $(xrandr -q | grep 'DP-2 connected') ]]; then
-  polybar ext &
-fi
--- scripts/wp.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-wall="${HOME}/.wall"
-
-DISPLAY=:0 feh --bg-fill --randomize --recursive "${wall}" 2>/dev/null
